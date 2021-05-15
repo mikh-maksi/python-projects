96fb4a8333abdd3741b91457b6fe665f6f30e9eb
Mikhail <newlevelcenter@gmail.com>
2021-05-15 14:40:25 +0300

    eventx

---
+++ snake/snake-tasks6.py
@@ -0,0 +1,39 @@
+from tkinter import Tk, Canvas # Подключаем модуль
+import random # Модуль генерирования случайных чисел
+
+WIDTH = 300
+HEIGHT= 300
+BACKCOLOR = "#003300"
+FONTSIZE1 = "Arial 15"
+FONTSIZE2 = "Arial 20"
+
+
+def clicked(event):
+    print(event.x)
+    print(event.y)
+    if (event.x<150):
+        c.itemconfigure(restart_text, state='hidden')
+        c.itemconfigure(game_over_text, state='hidden')
+
+root = Tk()
+root.title("My Game")
+
+
+
+c = Canvas(root, width=WIDTH, height=HEIGHT, bg=BACKCOLOR)
+c.grid()
+
+c.focus_set()
+
+
+game_over_text = c.create_text(WIDTH/2, HEIGHT/2, text="GAME OVER!",
+                               font=FONTSIZE1, fill='red',
+                               state='normal')
+restart_text = c.create_text(WIDTH/2, HEIGHT-HEIGHT/3,
+                             font=FONTSIZE2,
+                             fill='white',
+                             text="Click here to restart",
+                             state='normal')
+
+c.tag_bind(restart_text, "<Button-1>", clicked)
+root.mainloop()
